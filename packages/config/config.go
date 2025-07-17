7c7b37be07ff529e3fcba80fb9964ca341f74c00
souvikjs01 <souvik741156@gmail.com>
2025-07-17 22:46:31 +0530

    update config

--- packages/config/config.go
+++ packages/config/config.go
@@ -8,9 +8,18 @@ import (
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("error loading env file")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("error loading env file")
+	// }
+
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("No .env file found — falling back to system environment variables")
+		} else {
+			log.Println(".env file loaded successfully")
+		}
 	}
 }
 
